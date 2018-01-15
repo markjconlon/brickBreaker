c30d746886eec17a43d410bfdd13bdbac4abec95
markjconlon <markjconlon@gmail.com>
2018-01-15 18:19:37 -0500

    added horizontal movement to the player paddle from left and right arrow keys

--- main.js
+++ main.js
@@ -12,6 +12,18 @@ document.addEventListener("DOMContentLoaded", function() {
   let paddleX = canvas.width/2 - paddleWidth/2;
   let paddleY = canvas.height-10;
 
+  document.addEventListener("keydown", function(e){
+    if (e.keyCode === 37) {
+      if (paddleX > 0) {
+        paddleX -= 8;
+      }
+    } else if (e.keyCode === 39) {
+      if (paddleX < canvas.width - paddleWidth) {
+        paddleX += 8;
+      }
+    }
+  });
+
   function draw(){
     // board
     drawRect(0,0,canvas.width,canvas.height,"blue")
